d71358cb86b833ff97c3e46d62e885a6f211751e
René Thomas Lottes <rene.lottes@gmx.de>
2024-01-15 08:17:21 +0100

    Set privacy url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor() {}
+
+  ngOnInit(): void {
+    // @ts-ignore
+    cordova.plugins.health.setPrivacyPolicyURL("https://example.com", () => {
+      console.info("success")
+    }, (e: any) => {
+      console.info("Err:", e)
+    });
+  }
 }
