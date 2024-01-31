df2f4e01dac3e52c801f510a9caabc0ecd0cddd8
René Thomas Lottes <rene.lottes@gmx.de>
2024-01-31 08:20:29 +0100

    Testing health plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Health, HealthData, HealthDataType } from '@awesome-cordova-plugins/health/ngx';
 
 @Component({
   selector: 'app-root',
@@ -23,8 +24,53 @@ export class AppComponent implements OnInit {
     // @ts-ignore
     cordova.plugins.health.launchPrivacyPolicy(() => {
       console.info("success")
+      this.requestAuth({
+        read: ['steps', 'calories', 'calories.basal', 'calories.active']
+      });
     }, (e: any) => {
       console.info("Err:", e)
     });
+  }
+
+
+  private requestAuth(data : HealthDataType) {
+    // @ts-ignore
+    cordova.plugins.health.requestAuthorization(data, (res) => {
+      console.info("Health-Auth:", res)
+
+      // @ts-ignore
+      cordova.plugins.health.query({
+        startDate: new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 3),
+        endDate: new Date(),
+        dataType: 'calories'
+      }, (s:any) => {
+        console.info("Success", s)
+      }, (s:any) => {
+        console.info("Error", s)
+      })
+
+    },((err: any) => console.error(err)))
+  }
+
+  private query(dStart: Date, dEnd: Date, type: string, limit = 1000): Promise<HealthData[]> {
+    return new Promise((resolve, reject) => {
+      const successCallback = (data: any) => {
+        console.info("Query-data:", dStart, dEnd, type, data);
+        resolve(data);
+      };
+      const errorCallback = (error: any) => {
+        console.error("Could not query health data.", dStart, dEnd, type)
+        reject(error);
+      };
+
+      // @ts-ignore
+      cordova.plugins.health.query({
+          dataType: type,
+          startDate: dStart,
+          endDate: dEnd,
+          filterOutUserInput: true,
+          limit: limit
+        }, successCallback, errorCallback)
+      })
   }
 }
